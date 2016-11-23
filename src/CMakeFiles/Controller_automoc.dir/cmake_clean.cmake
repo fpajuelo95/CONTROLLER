0a827312ea85016901f2e8fc1b4695f2827d11e9
Juan Luis Rastrollo Guerrero <jurastrol@alumnos.unex.es>
2016-11-23 10:10:44 +0100

    version final controller

--- src/CMakeFiles/Controller_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Controller_automoc.cpp"
-  "CMakeFiles/Controller_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Controller_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
